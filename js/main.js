97df63787a3e1427ca19318aac8ae01e50a1e7ba
peoplecouple <peoplecouple@naver.com>
2022-07-25 09:22:39 +0900

    220725

--- js/main.js
+++ js/main.js
@@ -6,14 +6,20 @@ $(function () {
     pauseOnHover: false,
     pauseOnFocus: false,
   })
+ 
+  $(' .main_visual .control i:nth-child(1)').on('click', function () {
+    $('.main_visual .main_slide').slick('slickPause')
+  })
+
+  $(' .main_visual .control i:nth-child(2)').on('click', function () {
+    $('.main_visual .main_slide').slick('slickPlay')
+  })
 
   $('.main_visual .main_tab .tab_menu>div').on('click', function () {
     $(this).addClass('on').siblings().removeClass('on')
     var idx = $(this).index()
     $('.main_visual .main_tab .tab_form').eq(idx).addClass('on').siblings().removeClass('on')
   })
-
-
 
   $('.main_tab .tab_form .top .trip_way div').on('click', function () {
     $(this).addClass('on').siblings().removeClass('on')
@@ -23,6 +29,7 @@ $(function () {
     $(this).toggleClass('on')
     $('.main_tab .tab_form .top .group_check p').toggleClass('on')
   })
+
 
   $('.lowest .lowest_slide').slick({
     arrows: false,
@@ -44,9 +51,24 @@ $(function () {
     e.preventDefault();
     $(this).addClass('on').parent().parent().siblings().find('p').removeClass('on')
 
+    if ($(this).parent().find('img:nth-child(1)').hasClass('on')){
+      $(this).parent().find('img:nth-child(1)').removeClass('on')
+      $(this).parent().find('img:nth-child(2)').addClass('on')
+      $(this).parent().parent().siblings().find('img:nth-child(2)').removeClass('on')
+      $(this).parent().parent().siblings().find('img:nth-child(1)').addClass('on')
+    }else {
+      $(this).parent().find('img:nth-child(2)').removeClass('on')
+      $(this).parent().find('img:nth-child(1)').addClass('on')
+      $(this).parent().parent().siblings().find('img:nth-child(2)').removeClass('on')
+      $(this).parent().parent().siblings().find('img:nth-child(1)').addClass('on')
+
+    }
+   
     var idx = $(this).parent().parent().index();
     $('.service .right figure').eq(idx).addClass('on').siblings().removeClass('on')
   })
+
+
 
   $('.promo .left_slider').slick({
     arrows: false,
